419e38e67e6b1b19b8a4cb3ab7f9261de84f27ee
UmitKok21 <umit.kok21@gmail.com>
2022-08-07 15:41:57 +0300

    FPS camera , player movement added.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public float jumpHeight = 3f;
+    public float groundDistance = 0.4f;
+    public float gravity = -9.81f;
+    public float speed;
+    public LayerMask groundMask;
+    Vector3 velocity;
+    Vector3 move;
+    bool isGrounded;
+    float x;
+    float z;
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
