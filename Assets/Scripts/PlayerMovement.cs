b06ab9d3e035ce22d42c91f61430ec2c2439323e
Fatih Cesur <fc.fatihcesur@gmail.com>
2022-08-08 20:30:53 +0300

    Player movement changed.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
-    public Transform groundCheck;
-    public float jumpHeight = 3f;
-    public float groundDistance = 0.4f;
-    public float gravity = -9.81f;
-    public float speed;
-    public LayerMask groundMask;
+    [Header("Character Controller")]
+    [SerializeField] private CharacterController controller;
+
+    [Header("Character Variables")]
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float crouchingHeight = 1.25f;
+    [SerializeField] private float standingHeight = 3.8f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float speed;
+
+
+    [Header("Ground Variables")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float groundDistance = 0.4f;
+
+    bool isGrounded;
+    bool isCrouching;
+    bool isSprinting;
+
     Vector3 velocity;
     Vector3 move;
-    bool isGrounded;
+
     float x;
     float z;
+
+
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
@@ -23,10 +38,44 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = -2f;
         }
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded || Input.GetButtonDown("Jump") && isCrouching)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+
+
+        // Sprinting
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+        {
+            isSprinting = true;
+            speed = speed * 2;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.LeftShift) && isGrounded)
+        {
+            speed = speed / 2;
+        }
+
+        //Crouching
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
+        {
+            isCrouching = true;
+            speed = speed / 2;
+            controller.height = crouchingHeight;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl) && controller.height == crouchingHeight)
+        {
+            isCrouching = false;
+            speed = speed * 2;
+            controller.height = standingHeight;
+
+        }
+
+        Debug.Log(speed);
+
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
         move = transform.right * x + transform.forward * z;
