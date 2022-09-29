abedc4603cd633fc54d9df315292ec685f3b8131
UmitKok21 <umit.kok21@gmail.com>
2022-09-29 16:02:33 +0300

    Pool tests bullet tracks and gun.cs updated

--- Assets/Scripts/Managers/Gun/Gun.cs
+++ Assets/Scripts/Managers/Gun/Gun.cs
@@ -9,6 +9,8 @@ public class Gun : MonoBehaviour
     private Camera fpsCamera;
     public ParticleSystem muzzleFlash;
     public Transform muzzleTransform;
+    public GameObject impactEffect;
+    RaycastHit hit;
     private void Start()
     {
         fpsCamera = Camera.main;
@@ -25,18 +27,28 @@ public class Gun : MonoBehaviour
     }
     void Shoot()
     {
-        RaycastHit hit;
         muzzleFlash.Play();
         if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
             Enemy enemy = hit.transform.GetComponent<Enemy>();
             if (enemy != null)
-            {
-                
+            {               
                 enemy.TakeDamage(damage);
-            }       
+            }
+            if (hit.transform.gameObject!=null)
+            {
+                ShowTrack();
+            }
+            
+            //Pool.instance.ShowTrack(hit.transform.position, Quaternion.LookRotation(hit.normal), rateOfFire);
         }
 
 
+    }
+    void ShowTrack()
+    {
+        GameObject temp;
+        temp = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        temp.transform.SetParent(hit.transform);
     }
 }
